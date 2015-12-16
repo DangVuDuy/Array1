9c50f7e755e3cfe8f00faab93d83eb7e45107207
DangVuDuy <vuduy081293@gmail.com>
2015-12-16 22:30:17 +0700

    Initial Commit

---
+++ QueryArray/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QueryArray
+//
+//  Created by Duy Dang on 12/16/15.
+//  Copyright © 2015 Duy Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
